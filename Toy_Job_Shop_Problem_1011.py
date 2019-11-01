3ba5276cd3d65082c12a9a6789f26b428b42bd3d
tamastkovacs <57113850+tamastkovacs@users.noreply.github.com>
2019-11-01 11:24:56 +0000

    Update Toy_Job_Shop_Problem_1011.py

--- Toy_Job_Shop_Problem_1011.py
+++ Toy_Job_Shop_Problem_1011.py
@@ -82,6 +82,7 @@ imported_data_df["Transition_Date_Start"] = org_1_5_transition_Date_Start
 #modify the transition start date for org 6 and 7
 imported_data_df.loc[5:6,"Transition_Date_Start"] = imported_data_df.loc[5:6,'Task 2 Completion date']
 
+
 #define Org2 Transition phase start
 org_2_task3_startdate = imported_data_df.loc[1,"Transition_Date_Start"]
 #define Org_4 Transition phase start
